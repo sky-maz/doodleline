ad07b7e60d7d3f39475934480d5c5781de4745d5
Angel Sky <hey.angelzama@gmail.com>
2022-04-04 00:13:10 -0600

    fix(styles): Update css to sass imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import '../styles/globals.scss';
+import '../sass/globals.scss';
 
 const App = ({ Component, pageProps }: AppProps) => (
 	<Component {...pageProps} />
