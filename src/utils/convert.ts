38d6c3ab623e02b66270deebfd031281a5df1335
Angel Sky <hey.angelzama@gmail.com>
2022-05-13 22:06:14 -0600

    chore(utils): Relocate utils folder

--- src/utils/convert.ts
+++
@@ -1,7 +0,0 @@
-export const asTime = (time: number): string => {
-	const mins = Math.floor(time / 60);
-	const segs = time % 60;
-	const minsString = mins < 10 ? `0${mins}` : `${mins}`;
-	const segsString = segs < 10 ? `0${segs}` : `${segs}`;
-	return `${minsString}:${segsString}`;
-};
